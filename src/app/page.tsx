301698f68c0f12a2bbcfb6272c1d4ff466f727ce
OMI786I <sohrawardy1998@gmail.com>
2025-02-27 03:20:19 +0600

    added shadcn and a basic navbar page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <p>Welcome page or home page</p>
+      <Button variant="destructive">Click me</Button>
     </>
   );
 }
